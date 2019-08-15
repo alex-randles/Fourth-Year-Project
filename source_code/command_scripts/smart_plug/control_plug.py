64606fcdc33c5d1fa093736d25f8b6435af9dd59
Alex Randles <alexrandles0@gmail.com>
2019-08-15 16:26:11 +0100

    .

--- source_code/command_scripts/smart_plug/control_plug.py
+++ source_code/command_scripts/smart_plug/control_plug.py
@@ -5,8 +5,6 @@ import subprocess
 import hotword
 
 def main(spoken_words):
-    # i have omitted this script as it was written by the matrix creator team
-    # i have been granted permission by them to use it
     if "on" in spoken_words:
         subprocess.Popen(["node /home/pi/zigbee_on.js &"], shell=True)
     else:
